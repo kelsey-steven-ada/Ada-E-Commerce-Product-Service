2a03d6e35663e7043f1300075f1033f1b643e210
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2026-07-31 13:36:29 -0700

    Update default region to us-west-2

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ def aws_credentials():
     os.environ["TABLE_NAME"] = "test-table"
     os.environ["KEY_NAME"] = "product_key"
     os.environ["BUCKET_NAME"] = "test-bucket"
-    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+    os.environ["AWS_DEFAULT_REGION"] = "us-west-2"
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["S3_DEFAULT_KEY"] = "S3_DEFAULT_KEY"
